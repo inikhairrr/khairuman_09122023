80f39abd3b2112bc4bd4c8f6acb53f72f27c26ee
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-09 15:01:46 +0700

    database

---
+++ database/migrations/2023_12_09_073043_create_pegawais_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id('pegawai_id');
+            $table->string('pegawai_nama', 50);
+            $table->tinyInteger('pegawai_umur');
+            $table->longText('pegawai_alamat');
+            $table->string('foto', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
